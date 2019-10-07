92b422628bc68d774c2fa10c7d12f3f2912a16c7
GadkiyAbram <aabramovskii@gmail.com>
2019-10-07 18:47:43 +0500

    Laravel Tutorial e11 - done Input validating done + |regex:/^[a-zA-Z]/ (deleted for now) Storing data into two separate vars, displaying accordingly

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -1,32 +1,64 @@
 @extends('layout')
 
+@section('title', 'Customer List')
+
 @section('content')
-	<h1>Customers</h1>
+	<div class="row">
+		<div class="col-12">
+			<h1>Customer List</h1>
+		</div>
+	</div>
 
-	<form action="customers" method="POST" class="pb-5">
-		<p>Name:</p>
-		<div class="input-group pb-2">
-			<input type="text" name="name" value="{{ old('name') }}">
-			<div>{{ $errors->first('name') }}</div>
+	<div class="row">
+		<col-12>
+			<form action="customers" method="POST">
+				<div class="form-group pb-2">
+					<label for="name">Name</label>
+					<input type="text" name="name" value="{{ old('name') }}" class="form-control">
+					<div>{{ $errors->first('name') }}</div>
+				</div>
+
+				<div class="form-group pb-2">
+					<label for="email">Email</label>
+					<input type="text" name="email" value="{{ old('email') }}" class="form-control">
+					<div>{{ $errors->first('email') }}</div>
+				</div>
+
+				<div class="form-group">
+					<label for="active">Status</label>
+					<select name="active" id="active" class="form-control">
+						<option value="" disabled>Select customer status</option>
+						<option value="1">Active</option>
+						<option value="0">Inactive</option>
+					</select>
+				</div>
+
+				<button type="submit" class="btn btn-primary">Add Customer</button>
+
+				@csrf
+			</form>
+		</col-12>
+	</div>
+
+	<hr>
+	<div class="row">
+		<div class="col-6">
+			<h3>Active Customers</h3>
+				<ul>
+					@foreach ($activeCustomers as $aCustomer)
+						<li>{{ $aCustomer->name }} <span class="text-muted">({{ $aCustomer->email }})</span></li>
+					@endforeach
+				</ul>
 		</div>
 
-
-		<p>Email:</p>
-		<div class="input-group pb-2">
-			<input type="text" name="email" value="{{ old('email') }}">
-			<div>{{ $errors->first('email') }}</div>
+		<div class="col-6">
+			<h3>Inactive Customers</h3>
+				<ul>
+					@foreach ($inactiveCustomers as $iCustomer)
+						<li>{{ $iCustomer->name }} <span class="text-muted">({{ $iCustomer->email }})</span></li>
+					@endforeach
+				</ul>
 		</div>
+	</div>
 
-
-		<button type="submit">Add Customer</button>
-
-		@csrf
-	</form>
-
-
-	<ul>
-		@foreach ($customers as $customer)
-			<li>{{ $customer->name }} <span class="text-muted">({{ $customer->email }})</span></li>
-		@endforeach
-	</ul>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,29 +4,16 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>My First Project</title>
+	<title>@yield('title', 'Learning Laravel 5.8')</title>
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
 
-<ul class="nav">
-	<li class="nav-item">
-		<a class="nav-link active" href="/">Home</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="about">About Us</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="contact">Contact Us</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="customers">Customers</a>
-	</li>
-</ul>
-
 <div class="container">
+	@include('nav')
+
 	@yield('content')
 </div>
 
