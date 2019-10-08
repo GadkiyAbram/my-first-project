84957070b824be3d7d61e42ae1a221f2d6f5d1e4
GadkiyAbram <aabramovskii@gmail.com>
2019-10-08 10:42:49 +0500

    Laravel Tutorial e14 - done Input validating done + |regex:/^[a-zA-Z]/ (deleted for now) (template) Added Company input field, adding customers accordingly

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Company;
 use App\Customer;
 use Illuminate\Http\Request;
 
@@ -9,10 +10,11 @@ class CustomersController extends Controller
 {
     public function list()
     {
-        $activeCustomers = Customer::where('active', 1)->get();
-        $inactiveCustomers = Customer::where('active', 0)->get();
+        $activeCustomers = Customer::active()->get();
+        $inactiveCustomers = Customer::inactive()->get();
+        $companies = Company::all();
 
-        return view('internals.customers', compact('activeCustomers', 'inactiveCustomers'));
+        return view('internals.customers', compact('activeCustomers', 'inactiveCustomers', 'companies'));
     }
 
     public function store()
@@ -20,14 +22,11 @@ class CustomersController extends Controller
         $data = request()->validate([
             'name' => 'required|min:3',
             'email' => 'required|email',
-            'active' => 'required'
+            'active' => 'required',
+            'company_id' => 'required'
         ]);
 
-        $customer = new Customer();
-        $customer->name = request('name');
-        $customer->email = request('email');
-        $customer->active = request('active');
-        $customer->save();
+        Customer::create($data);
 
         return back();
     }
--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -33,6 +33,16 @@
 					</select>
 				</div>
 
+				<div class="form-group">
+					<label for="company_id">Company</label>
+					<select name="company_id" id="company_id" class="form-control">
+						@foreach($companies as $company)
+							<option value="{{ $company->id }}">{{ $company->name }}</option>
+						@endforeach
+
+					</select>
+				</div>
+
 				<button type="submit" class="btn btn-primary">Add Customer</button>
 
 				@csrf
@@ -58,6 +68,21 @@
 						<li>{{ $iCustomer->name }} <span class="text-muted">({{ $iCustomer->email }})</span></li>
 					@endforeach
 				</ul>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-12">
+			@foreach($companies as $company)
+				<h3>{{ $company->name }}</h3>
+
+				<ul>
+					@foreach($company->customers as $customer)
+						<li>{{ $customer->name }}</li>
+					@endforeach
+				</ul>
+
+			@endforeach
 		</div>
 	</div>
 
